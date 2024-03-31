01e45a6142c20b8d535111e78224abcabe02cd76
Khalaq-Shah <khalaqshah04@gmail.com>
2024-03-31 16:54:49 +0530

    Added send, receive, and accept connections to Search Fragment.

--- app/src/main/java/com/krskhalaq/connecthub/SearchFragmentAdapter.kt
+++ app/src/main/java/com/krskhalaq/connecthub/SearchFragmentAdapter.kt
@@ -2,6 +2,7 @@ package com.krskhalaq.connecthub
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -32,18 +33,63 @@ class SearchFragmentAdapter(private val context: Context, private val userList: 
 
         fun bind(position: Int) {
             searchItemTV.text = userList[position].name
+            var received = false
+            var connected = false
 
-            SignUpActivity.dbFirebase.getReference("Users").child(SignUpActivity.uId).child("ConnectReqSent").addValueEventListener(object : ValueEventListener {
+            SignUpActivity.dbFirebase.getReference("Users").child(SignUpActivity.uId).child("Connections").addValueEventListener(object : ValueEventListener {
                 @SuppressLint("SetTextI18n")
                 override fun onDataChange(snapshot: DataSnapshot) {
                     for (ds in snapshot.children) {
                         if (ds.value.toString() == userList[position].id) {
-                            searchItemBtn.text = "Pending"
+                            searchItemBtn.text = "Connected"
                             searchItemBtn.isEnabled = false
+                            connected = true
                         }
                     }
-                    searchItemBtn.setOnClickListener {
-                        sendConnectRequest(userList[position])
+                    if (!connected) {
+                        SignUpActivity.dbFirebase.getReference("Users").child(SignUpActivity.uId).child("ConnectReqReceived").addValueEventListener(object : ValueEventListener {
+                            @SuppressLint("SetTextI18n")
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                for (ds in snapshot.children) {
+                                    if (ds.value.toString() == userList[position].id) {
+                                        searchItemBtn.text = "Accept"
+                                        searchItemBtn.isEnabled = true
+                                        received = true
+                                    }
+                                }
+                                if (received) {
+                                    searchItemBtn.setOnClickListener {
+                                        acceptConnectRequest(userList[position])
+                                    }
+                                }
+                                else {
+                                    SignUpActivity.dbFirebase.getReference("Users").child(SignUpActivity.uId).child("ConnectReqSent").addValueEventListener(object : ValueEventListener {
+                                        @SuppressLint("SetTextI18n")
+                                        override fun onDataChange(snapshot: DataSnapshot) {
+                                            for (ds in snapshot.children) {
+                                                if (ds.value.toString() == userList[position].id) {
+                                                    searchItemBtn.text = "Pending"
+                                                    searchItemBtn.isEnabled = false
+                                                }
+                                            }
+                                            searchItemBtn.setOnClickListener {
+                                                sendConnectRequest(userList[position])
+                                            }
+                                        }
+
+                                        override fun onCancelled(error: DatabaseError) {
+
+                                        }
+
+                                    })
+                                }
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+
+                            }
+
+                        })
                     }
                 }
 
@@ -52,19 +98,136 @@ class SearchFragmentAdapter(private val context: Context, private val userList: 
                 }
 
             })
+
+
+        }
+
+        private fun acceptConnectRequest(user: User) {
+            var dbRef = SignUpActivity.dbFirebase.getReference("Users").child(SignUpActivity.uId).child("Connections")
+            dbRef.push().setValue(user.id).addOnCompleteListener {it1 ->
+                if (it1.isSuccessful) {
+                    Toast.makeText(context, "Accepting Connect Request!", Toast.LENGTH_SHORT).show()
+                    searchItemBtn.text = "Connected"
+                    searchItemBtn.isEnabled = false
+
+                    dbRef = SignUpActivity.dbFirebase.getReference("Users").child(user.id).child("Connections")
+                    dbRef.push().setValue(SignUpActivity.uId).addOnCompleteListener {it2 ->
+                        if (it2.isSuccessful) {
+//                            Toast.makeText(context, "Connect Request Sent!", Toast.LENGTH_SHORT).show()
+                            searchItemBtn.text = "Connected"
+                            searchItemBtn.isEnabled = false
+
+                            dbRef = SignUpActivity.dbFirebase.getReference("Users").child(SignUpActivity.uId).child("ConnectReqReceived")
+                            dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                                override fun onDataChange(snapshot: DataSnapshot) {
+                                    for (ds in snapshot.children) {
+                                        Log.i("SearchFragmentAdapter", ds.value.toString())
+                                        if (ds.value.toString() == user.id) {
+                                            Log.i("SearchFragmentAdapter", ds.value.toString())
+                                            ds.ref.removeValue()
+                                        }
+                                    }
+
+                                    dbRef = SignUpActivity.dbFirebase.getReference("Users").child(user.id).child("ConnectReqSent")
+                                    dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                                        override fun onDataChange(snapshot: DataSnapshot) {
+                                            for (ds in snapshot.children) {
+                                                Log.i("SearchFragmentAdapter", ds.value.toString())
+                                                if (ds.value.toString() == SignUpActivity.uId) {
+                                                    Log.i("SearchFragmentAdapter", ds.value.toString())
+                                                    ds.ref.removeValue()
+                                                }
+                                            }
+
+                                            Toast.makeText(context, "Accepted Connect Request!", Toast.LENGTH_SHORT).show()
+                                        }
+
+                                        override fun onCancelled(error: DatabaseError) {
+
+                                        }
+
+                                    })
+                                }
+
+                                override fun onCancelled(error: DatabaseError) {
+
+                                }
+
+                            })
+                        }
+                        else {
+                            Toast.makeText(context, "Failed to send request!", Toast.LENGTH_SHORT).show()
+                            searchItemBtn.text = "Accept"
+                            searchItemBtn.isEnabled = true
+                            dbRef = SignUpActivity.dbFirebase.getReference("Users").child(SignUpActivity.uId).child("Connections")
+                            dbRef.removeEventListener(object : ValueEventListener {
+                                override fun onDataChange(snapshot: DataSnapshot) {
+                                    for (ds in snapshot.children) {
+                                        if (ds.value == user.id) {
+                                            ds.ref.removeValue()
+                                        }
+                                    }
+                                }
+
+                                override fun onCancelled(error: DatabaseError) {
+
+                                }
+
+                            })
+                        }
+                    }
+                }
+                else {
+                    Toast.makeText(context, "Failed to send request!", Toast.LENGTH_SHORT).show()
+                    searchItemBtn.text = "Accept"
+                    searchItemBtn.isEnabled = true
+                }
+            }
         }
 
         @SuppressLint("SetTextI18n")
         private fun sendConnectRequest(user: User) {
-            val dbRef = SignUpActivity.dbFirebase.getReference("Users").child(SignUpActivity.uId).child("ConnectReqSent")
-            dbRef.push().setValue(user.id).addOnCompleteListener {
-                if (it.isSuccessful) {
-                    Toast.makeText(context, "Connect Request Sent!", Toast.LENGTH_SHORT).show()
+            var dbRef = SignUpActivity.dbFirebase.getReference("Users").child(SignUpActivity.uId).child("ConnectReqSent")
+            dbRef.push().setValue(user.id).addOnCompleteListener {it1 ->
+                if (it1.isSuccessful) {
+                    Toast.makeText(context, "Sending Connect Request!", Toast.LENGTH_SHORT).show()
                     searchItemBtn.text = "Pending"
                     searchItemBtn.isEnabled = false
+
+                    dbRef = SignUpActivity.dbFirebase.getReference("Users").child(user.id).child("ConnectReqReceived")
+                    dbRef.push().setValue(SignUpActivity.uId).addOnCompleteListener {it2 ->
+                        if (it2.isSuccessful) {
+                            Toast.makeText(context, "Connect Request Sent!", Toast.LENGTH_SHORT).show()
+                            searchItemBtn.text = "Pending"
+                            searchItemBtn.isEnabled = false
+                        }
+                        else {
+                            Toast.makeText(context, "Failed to send request!", Toast.LENGTH_SHORT).show()
+                            searchItemBtn.text = "Connect"
+                            searchItemBtn.isEnabled = true
+                            dbRef = SignUpActivity.dbFirebase.getReference("Users").child(SignUpActivity.uId).child("ConnectReqSent")
+                            dbRef.removeEventListener(object : ValueEventListener {
+                                override fun onDataChange(snapshot: DataSnapshot) {
+                                    for (ds in snapshot.children) {
+                                        if (ds.value == user.id) {
+                                            ds.ref.removeValue()
+                                        }
+                                    }
+                                }
+
+                                override fun onCancelled(error: DatabaseError) {
+
+                                }
+
+                            })
+                        }
+                    }
                 }
-                else
+                else {
                     Toast.makeText(context, "Failed to send request!", Toast.LENGTH_SHORT).show()
+                    searchItemBtn.text = "Connect"
+                    searchItemBtn.isEnabled = true
+                }
             }
         }
 
